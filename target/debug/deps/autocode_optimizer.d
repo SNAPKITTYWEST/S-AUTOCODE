86f19ec8ad808fc527d6a78ba0fbeba71c27688d
SNAPKITTYWEST <jessicalw34@gmail.com>
2026-07-06 02:39:48 -0600

    chore: add .gitignore, remove build artifacts

--- target/debug/deps/autocode_optimizer.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jessi\IdeaProjects\S-AUTOCODE\target\debug\deps\autocode_optimizer.d: src\optimizer\src\main.rs
-
-C:\Users\jessi\IdeaProjects\S-AUTOCODE\target\debug\deps\autocode_optimizer.exe: src\optimizer\src\main.rs
-
-src\optimizer\src\main.rs:
